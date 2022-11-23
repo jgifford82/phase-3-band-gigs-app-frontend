e58a9c25306dab68801aff911cf7c9fc26b7e697
Jennifer Gifford <jgifford82@gmail.com>
2022-11-23 13:41:17 -0700

    add image to about page

--- src/components/About.js
+++ src/components/About.js
@@ -12,6 +12,10 @@ const About = () => {
       >
         <p>See what shows are happening in your city!</p>
         <p>Bands can add their shows to the list!</p>
+        <img
+          src="https://images.pexels.com/photos/1749822/pexels-photo-1749822.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="Band Members Playing On Stage"
+        ></img>
       </Typography>
     </div>
   );
