c7576febf538725f7f9106d839d31d548fed1408
Jennifer Gifford <jgifford82@gmail.com>
2022-11-23 12:30:13 -0700

    set up navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./About";
+import GigsContainer from "./GigsContainer";
+// import BrowserRouter as Router for easier reference in code below.
+// use Routes instead of Switch, and wrap everything in Routes.
+// Keep NavBar within Router below so we can use the links.
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello world.</p>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/gigs" element={<GigsContainer />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
