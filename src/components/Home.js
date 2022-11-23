953953b88137cd11fc2ebc58c5e2865e16b56893
Jennifer Gifford <jgifford82@gmail.com>
2022-11-23 13:35:20 -0700

    add mui styling to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,20 @@
 import React from "react";
+import Typography from "@mui/material/Typography";
 
 const Home = () => {
   return (
     <div>
-      <img
-        src="https://images.pexels.com/photos/167636/pexels-photo-167636.jpeg"
-        alt="Singer Singing on Stage Beside Guitar Player and Bass Player"
-      ></img>
+      <Typography
+        variant="h3"
+        component="div"
+        sx={{ flexGrow: 1 }}
+        align="center"
+      >
+        <img
+          src="https://images.pexels.com/photos/167636/pexels-photo-167636.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="Singer Singing on Stage Beside Guitar Player and Bass Player"
+        ></img>
+      </Typography>
     </div>
   );
 };
