454f6aa8ee7d4678d72d2c97da0155701959cb07
Jennifer Gifford <jgifford82@gmail.com>
2022-11-23 12:30:54 -0700

    set up NavBar navigation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+// component property contains link so the button knows to act like a link (per mui.com third-party routing library). To= tells it where to go. This way we don't need event listeners. Link allows us to navigate without a page refresh.
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Band Gigs
+        </Typography>
+        <Button color="inherit" to="/" component={Link}>
+          Home
+        </Button>
+        <Button color="inherit" to="/about" component={Link}>
+          About
+        </Button>
+        <Button color="inherit" to="/events" component={Link}>
+          Gigs
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default NavBar;
