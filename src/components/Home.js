82c7ae8ad5574dddb51ede4c23bf909a5a106538
Jennifer Gifford <jgifford82@gmail.com>
2022-11-23 13:28:11 -0700

    add image to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <img
+        src="https://images.pexels.com/photos/167636/pexels-photo-167636.jpeg"
+        alt="Singer Singing on Stage Beside Guitar Player and Bass Player"
+      ></img>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
