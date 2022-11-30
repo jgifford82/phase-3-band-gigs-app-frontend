51da73ee1a77186a077450cddf9fb819dcd7ebe8
Jennifer Gifford <jgifford82@gmail.com>
2022-11-29 17:41:31 -0700

    set up controlled form for adding new gigs

--- src/components/GigsForm.js
+++ src/components/GigsForm.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from "@mui/material/Typography";
 
 const GigsForm = () => {
+  const initialValues = {
+    band: "",
+    date: "",
+    time: "",
+    venue: "",
+    price: "",
+  };
+
+  // This state sets the default form input values as an object with empty strings.
+  const [values, setValues] = useState(initialValues);
+
+  // This function handles all form inputs with a single onChange handler.
+  // Destructured name & value attributes from input fields to reference the key/value pairs when updating state.
+  // onChange prop added to each input to call handleInputChange
+  const handleInputChange = (e) => {
+    //const name = e.target.name
+    //const value = e.target.value
+    const { name, value } = e.target;
+
+    setValues({
+      ...values,
+      [name]: value,
+    });
+    console.log(values);
+  };
+
   return (
     <div>
       GigsForm
@@ -16,38 +42,38 @@ const GigsForm = () => {
             <span style={{ fontWeight: "bold" }}>Add New Gig:</span>
             <input
               type="text"
-              name="name"
+              name="band"
               placeholder="Band Name"
-              // value={values.title}
-              // onChange={handleInputChange}
+              value={values.band}
+              onChange={handleInputChange}
             />
             <input
               type="text"
               name="date"
               placeholder="Date"
-              // value={values.day}
-              // onChange={handleInputChange}
+              value={values.date}
+              onChange={handleInputChange}
             />
             <input
               type="text"
               name="time"
               placeholder="Time"
-              // value={values.time}
-              // onChange={handleInputChange}
+              value={values.time}
+              onChange={handleInputChange}
             />
             <input
               type="text"
               name="venue"
               placeholder="Venue"
-              // value={values.location}
-              // onChange={handleInputChange}
+              value={values.venue}
+              onChange={handleInputChange}
             />
             <input
               type="text"
               name="price"
               placeholder="Price"
-              // value={values.location}
-              // onChange={handleInputChange}
+              value={values.price}
+              onChange={handleInputChange}
             />
           </label>
           <input type="submit" value="Submit" />
