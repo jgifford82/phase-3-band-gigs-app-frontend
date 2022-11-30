5b8acb6c47ee92a3769c8dd260ecc6c10c6c9d62
Jennifer Gifford <jgifford82@gmail.com>
2022-11-29 17:04:35 -0700

    set up form for adding gigs

--- src/components/GigsForm.js
+++ src/components/GigsForm.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import Typography from "@mui/material/Typography";
 
 const GigsForm = () => {
   return (
-    <div>GigsForm</div>
-  )
-}
+    <div>
+      GigsForm
+      <Typography
+        variant="h6"
+        component="div"
+        sx={{ flexGrow: 1 }}
+        align="center"
+      >
+        <form>
+          <label>
+            <span style={{ fontWeight: "bold" }}>Add New Gig:</span>
+            <input
+              type="text"
+              name="name"
+              placeholder="Band Name"
+              // value={values.title}
+              // onChange={handleInputChange}
+            />
+            <input
+              type="text"
+              name="date"
+              placeholder="Date"
+              // value={values.day}
+              // onChange={handleInputChange}
+            />
+            <input
+              type="text"
+              name="time"
+              placeholder="Time"
+              // value={values.time}
+              // onChange={handleInputChange}
+            />
+            <input
+              type="text"
+              name="venue"
+              placeholder="Venue"
+              // value={values.location}
+              // onChange={handleInputChange}
+            />
+            <input
+              type="text"
+              name="price"
+              placeholder="Price"
+              // value={values.location}
+              // onChange={handleInputChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </Typography>
+    </div>
+  );
+};
 
-export default GigsForm
+export default GigsForm;
