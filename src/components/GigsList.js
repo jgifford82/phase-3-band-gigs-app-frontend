152f136d64e8e93cbeeee12af93cf4c35c387173
Jennifer Gifford <jgifford82@gmail.com>
2022-11-30 17:41:35 -0700

    add delete button click handler

--- src/components/GigsList.js
+++ src/components/GigsList.js
@@ -2,9 +2,14 @@ import React from "react";
 
 const GigsList = ({ gigs }) => {
   // console.log(gigs);
+
+  function handleDelete() {
+    console.log("clicked");
+  }
+
   const renderGigs = gigs.map((gig) => (
     <ul key={gig.id}>
-      <button>X</button>
+      <button onClick={handleDelete}>X</button>
       <span style={{ fontWeight: "bold" }}>{gig.band}</span>: {gig.date}{" "}
       {gig.time} at {gig.venue} for ${gig.price}
     </ul>
