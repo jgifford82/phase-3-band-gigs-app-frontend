e87a591a1a0f756aa29c673ab800d6b6e9ecdafa
Jennifer Gifford <jgifford82@gmail.com>
2022-12-04 13:28:52 -0700

    add cities route to browser router in app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./About";
 import EventsContainer from "./EventsContainer";
+import CitiesContainer from "./CitiesContainer";
 // import BrowserRouter as Router for easier reference in code below.
 // use Routes instead of Switch, and wrap everything in Routes.
 // Keep NavBar within Router below so we can use the links.
 
 const App = () => {
-  const [cities, setCities] = useState([]);
+  // const [cities, setCities] = useState([]);
 
-  console.log(cities);
+  // console.log(cities);
 
-  useEffect(() => {
-    fetch("http://localhost:9292/cities")
-      .then((res) => res.json())
-      //   .then((data) => console.log(data));
-      .then((data) => setCities(data));
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:9292/cities")
+  //     .then((res) => res.json())
+  //     //   .then((data) => console.log(data));
+  //     .then((data) => setCities(data));
+  // }, []);
 
   // This is an example of code for onDelete function:
   //   const newCities = cities.map((city) => {
@@ -35,6 +36,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/cities" element={<CitiesContainer />} />
         <Route path="/events" element={<EventsContainer />} />
       </Routes>
     </Router>
