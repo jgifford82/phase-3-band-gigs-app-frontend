5aa5972b55187cb482188dbe4525754635e2bb7f
Jennifer Gifford <jgifford82@gmail.com>
2022-12-05 18:26:19 -0700

    add click handler for each city

--- src/components/CitiesList.js
+++ src/components/CitiesList.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 const CitiesList = ({ cities }) => {
+  function handleClick() {
+    console.log("city clicked");
+  }
+
   const renderCities = cities.map((city) => (
     <ul key={city.id}>
-      <span style={{ fontWeight: "bold" }}>{city.name}</span>
+      <span onClick={handleClick} style={{ fontWeight: "bold" }}>
+        {city.name}
+      </span>
     </ul>
   ));
   return <div>CitiesList {renderCities}</div>;
