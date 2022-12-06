fde39f26dc481af05147ffa9f8d90c752bdaaec4
Jennifer Gifford <jgifford82@gmail.com>
2022-12-06 10:59:35 -0700

    use link instead of click handler

--- src/components/CitiesList.js
+++ src/components/CitiesList.js
@@ -1,17 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CitiesList = ({ cities }) => {
-  // This makes each city clickable and console logs the id of the clicked city.
-  function handleClick(id) {
-    console.log("city clicked");
-    console.log(id);
-  }
+  //  Don't need the click handler since Link makes it clickable and navigates to the city's events
+  // This makes each city clickable and console logs the id of the clicked city and the events associated with that city.
+  // function handleClick(id, events) {
+  //   console.log("city clicked");
+  //   console.log(id);
+  //   console.log(cities);
+  //   console.log(events);
+  // }
 
   const renderCities = cities.map((city) => (
     <ul key={city.id}>
-      <span onClick={() => handleClick(city.id)} style={{ fontWeight: "bold" }}>
+      <Link
+        to={`/cities/${city.id}`}
+        // onClick={() => handleClick(city.id, city.events)}
+        style={{ fontWeight: "bold" }}
+      >
         {city.name}
-      </span>
+      </Link>
     </ul>
   ));
   return <div>CitiesList {renderCities}</div>;
