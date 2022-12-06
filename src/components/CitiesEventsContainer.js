35e22504e5054ab4cfd24a3fb31cbab8332fc4e6
Jennifer Gifford <jgifford82@gmail.com>
2022-12-06 11:03:32 -0700

    add comments

--- src/components/CitiesEventsContainer.js
+++ src/components/CitiesEventsContainer.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 const CitiesEventsContainer = () => {
+  // map out cities events
+  // put delete event & update here
   return <div>CitiesEventsContainer</div>;
 };
 
