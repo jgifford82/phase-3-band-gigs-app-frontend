2920aa2940742030ac9cb7600aa33b03d1c158da
Jennifer Gifford <jgifford82@gmail.com>
2022-12-05 20:22:47 -0700

    update click handler to find city id

--- src/components/CitiesList.js
+++ src/components/CitiesList.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 const CitiesList = ({ cities }) => {
-  function handleClick() {
+  // This makes each city clickable and console logs the id of the clicked city.
+  function handleClick(id) {
     console.log("city clicked");
+    console.log(id);
   }
 
   const renderCities = cities.map((city) => (
     <ul key={city.id}>
-      <span onClick={handleClick} style={{ fontWeight: "bold" }}>
+      <span onClick={() => handleClick(city.id)} style={{ fontWeight: "bold" }}>
         {city.name}
       </span>
     </ul>
