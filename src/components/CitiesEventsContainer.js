c298d81b627a728566fd89deda8d51aac90696bb
Jennifer Gifford <jgifford82@gmail.com>
2022-12-06 21:51:41 -0700

    delete unnecessary component since code was moved up to App component

--- src/components/CitiesEventsContainer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const CitiesEventsContainer = () => {
-  // map out cities events
-  // put delete event & update here
-  return <div>CitiesEventsContainer</div>;
-};
-
-export default CitiesEventsContainer;
