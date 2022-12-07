55c52562b5293f7eb26a7c1ebb1d2bfa3a485063
Jennifer Gifford <jgifford82@gmail.com>
2022-12-07 11:46:35 -0700

    destructure id value from useParams

--- src/components/CitiesEventsList.js
+++ src/components/CitiesEventsList.js
@@ -7,7 +7,8 @@ const CitiesEventsList = ({ cities }) => {
   // map out cities events
   // put delete event & update here
 
-  const id = useParams();
+  // useParams returns an object with key value pairs, so need to destructure the id value out of the useParams object
+  const { id } = useParams();
   // console.log(id);
 
   // need to find all events with city id that equals the params id
@@ -17,13 +18,17 @@ const CitiesEventsList = ({ cities }) => {
 
   // Need to play around with this more. When a city is clicked, it's only showing the events that match city id 12
   useEffect(() => {
-    const find = cities.find((id) => id === id);
+    const find = cities.find((city) => city.id == id);
+    // console.log(cities);
     // console.log(find);
+    // console.log(id);
     // console.log(find.events);
     setEvents(find.events);
-  }, [events]);
+  }, []);
 
-  console.log(events);
+  // , [events]
+
+  // console.log(events);
 
   const renderEvents = events.map((event) => (
     <ul key={event.id}>
