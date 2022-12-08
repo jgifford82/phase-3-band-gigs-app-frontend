ac990d67a81cf466542682ed813b094fc5135da5
Jennifer Gifford <jgifford82@gmail.com>
2022-12-07 20:35:18 -0700

    set up city form & styling

--- src/components/CitiesForm.js
+++ src/components/CitiesForm.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import Typography from "@mui/material/Typography";
 
 const CitiesForm = () => {
   return (
-    <div>CitiesForm</div>
-  )
-}
+    <div>
+      CitiesForm
+      <Typography
+        variant="h6"
+        component="div"
+        sx={{ flexGrow: 1 }}
+        align="center"
+      >
+        <form>
+          <label>
+            <span style={{ fontWeight: "bold" }}>Add New City:</span>
+            <input
+              type="text"
+              name="city"
+              placeholder="City"
+              // value={values.band}
+              // onChange={handleInputChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </Typography>
+    </div>
+  );
+};
 
-export default CitiesForm
+export default CitiesForm;
