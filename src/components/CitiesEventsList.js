502b599d39ec7dec411f44a08af2c702f8bc81c4
Jennifer Gifford <jgifford82@gmail.com>
2022-12-09 21:49:18 -0700

    set up delete click handler

--- src/components/CitiesEventsList.js
+++ src/components/CitiesEventsList.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 
-const CitiesEventsList = ({ cities }) => {
+const CitiesEventsList = ({ cities, onDeleteEvent }) => {
   // put post, delete, & patch here
 
   // useParams returns object with key/value pairs. destructured the id value to use it in foundCity variable
@@ -15,11 +15,20 @@ const CitiesEventsList = ({ cities }) => {
 
   const renderEvents = foundCity.events.map((event) => (
     <ul key={event.id}>
-      <button>X</button>
+      <button onClick={(e) => handleDeleteClick(e, event)}>X</button>
       <span style={{ fontWeight: "bold" }}>{event.band}</span>: {event.date} //{" "}
       {event.time} at {event.venue} for ${event.price}
     </ul>
   ));
+
+  function handleDeleteClick(e, event) {
+    fetch(`http://localhost:9292/events/${event.id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      // .then((data) => console.log(data));
+      .then((deletedEvent) => onDeleteEvent(deletedEvent));
+  }
 
   return <div>CitiesEventsList {renderEvents}</div>;
 };
