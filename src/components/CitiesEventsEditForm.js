1a5e696a3002cc5529388960459688c9a3a1841a
Jennifer Gifford <jgifford82@gmail.com>
2022-12-11 15:42:40 -0700

    set up form in CitiesEventsEditForm component

--- src/components/CitiesEventsEditForm.js
+++ src/components/CitiesEventsEditForm.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import Typography from "@mui/material/Typography";
 
 const CitiesEventsEditForm = () => {
   return (
-    <div>CitiesEventsEditForm</div>
-  )
-}
+    <div>
+      CitiesEventsEditForm
+      <Typography
+        variant="h6"
+        component="div"
+        sx={{ flexGrow: 1 }}
+        align="center"
+      >
+        {/* onSubmit={handleSubmit} */}
+        <form>
+          <label>
+            <span style={{ fontWeight: "bold" }}>Update Existing Event:</span>
+            <input
+              type="text"
+              name="band"
+              placeholder="Band Name"
+              //   value={values.band}
+              //   onChange={handleInputChange}
+            />
+            <input
+              type="text"
+              name="date"
+              placeholder="Date format YYYY-MM-DD"
+              //   value={values.date}
+              //   onChange={handleInputChange}
+            />
+            <input
+              type="text"
+              name="time"
+              placeholder="Time"
+              //   value={values.time}
+              //   onChange={handleInputChange}
+            />
+            <input
+              type="text"
+              name="venue"
+              placeholder="Venue"
+              //   value={values.venue}
+              //   onChange={handleInputChange}
+            />
+            <input
+              type="text"
+              name="price"
+              placeholder="Price (number only, no $)"
+              //   value={values.price}
+              //   onChange={handleInputChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </Typography>
+    </div>
+  );
+};
 
-export default CitiesEventsEditForm
+export default CitiesEventsEditForm;
