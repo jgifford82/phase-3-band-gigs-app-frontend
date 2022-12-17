e8495ab1b3092c50cecc36aff89c7076c3dbd285
Jennifer Gifford <jgifford82@gmail.com>
2022-12-16 21:51:39 -0700

    change renderEvents variable from const to let & add condition renderEvents if foundCity is true

--- src/components/CitiesEventsList.js
+++ src/components/CitiesEventsList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import CitiesEventsForm from "./CitiesEventsForm";
 import ShowCityEvent from "./ShowCityEvent.js";
@@ -15,20 +15,36 @@ const CitiesEventsList = ({
   const { id } = useParams();
   // console.log(id);
 
+  // console.log(cities);
+  const [foundCity2, setFoundCity2] = useState(null);
+
+  // create useeffect with cities in the dependency array. each time cities change, find the city with correspondin city id
+  useEffect(() => {
+    setFoundCity2(cities.find((city) => city.id == id));
+    // console.log(foundCity2);
+    // console.log(cities.find((city) => city.id == id));
+  }, [cities]);
+
+  console.log(foundCity2);
+
   // find all events with city id that equals the params id
   const foundCity = cities.find((city) => city.id == id);
   // console.log(foundCity);
   // console.log(foundCity.events);
 
   // Map over the events for the specific city whose page is being viewed and pass down the event info to ShowCityEvent component.
-  const renderEvents = foundCity.events.map((event) => (
-    <ShowCityEvent
-      key={event.id}
-      event={event}
-      onDeleteClick={handleDeleteClick}
-      onEditEvent={onEditEvent}
-    />
-  ));
+  let renderEvents;
+  if (foundCity) {
+    renderEvents = foundCity.events.map((event) => (
+      <ShowCityEvent
+        key={event.id}
+        event={event}
+        onDeleteClick={handleDeleteClick}
+        onEditEvent={onEditEvent}
+      />
+    ));
+  }
+  // if no foundCity, returns error undefined when refreshing a city's events page
 
   function handleDeleteClick(e, event) {
     fetch(`http://localhost:9292/events/${event.id}`, {
